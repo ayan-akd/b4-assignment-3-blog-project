a9d31ed895b9a7967d68fc33c19daec6b3e09e52
ayan-akd <ayankumar.akd@gmail.com>
2024-12-20 14:35:47 +0600

    changed an error message

--- src/app/errors/handleZodError.ts
+++ src/app/errors/handleZodError.ts
@@ -11,7 +11,7 @@ export const handleZodError = (err: ZodError): TGenericErrorResponse => {
     const statusCode = 400;
     return {
       statusCode,
-      message: 'Zod Validation Error',
+      message: 'Validation Error',
       errorSources,
     }
   };
